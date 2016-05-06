1a9def0f521b97b82836ba0f44272aaa65b74600
Sergey Kholodniy <sekh@ciklum.com>
2016-05-06 18:03:13 +0300

    Temporary commit

--- test/page.object.example.js
+++ test/page.object.example.js
@@ -1,12 +1,12 @@
-var PageObject = require('page-object');
-
-var SearchPage = PageObject.extend({
-    PageObject.url('http://www.google.com')
-
-var searchBox = PageObject.textbox('q');
-var searchButton = PageObject.button('btnG');
-});
-
-var ResultsPage = PageObject.extend({
-    var title = PageObject.title();
-});
+// var PageObject = require('page-object');
+//
+// var SearchPage = PageObject.extend({
+//     PageObject.url('http://www.google.com')
+//
+// var searchBox = PageObject.textbox('q');
+// var searchButton = PageObject.button('btnG');
+// });
+//
+// var ResultsPage = PageObject.extend({
+//     var title = PageObject.title();
+// });
